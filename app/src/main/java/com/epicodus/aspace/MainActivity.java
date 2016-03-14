586ad10a798f61f5858123cf7654028a564b30f0
Jeffrey Ruder and Nathan Romike <nathanromike@gmail.com>
2016-03-14 09:27:34 -0700

    Add initial login page

--- app/src/main/java/com/epicodus/aspace/MainActivity.java
+++ app/src/main/java/com/epicodus/aspace/MainActivity.java
@@ -2,12 +2,24 @@ package com.epicodus.aspace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Squirrels!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
